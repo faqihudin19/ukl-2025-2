2cc2c2dc2dc4395337b0e79b332f1c70cdea791c
faqihudin19 <faq19510@gmail.com>
2025-11-03 11:47:29 +0700

    Add files via upload

---
+++ soalUKL2.java
@@ -0,0 +1,16 @@
+package soal1;
+
+import java.util.Scanner;
+public class soalUKL2 {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("masukkan bilangan: ");
+    int bilangan = input.nextInt();
+
+    if (bilangan % 2 == 0){
+        System.out.println(bilangan + " adalah bilangan genap");
+    } else {
+        System.out.println(bilangan + " adalah bilangan ganjil");
+    }
+}
+}
